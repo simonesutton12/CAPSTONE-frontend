106547cddad817bb68529dd6e9209cd091f4ec76
Simone <simonesutton94@gmail.com>
2025-02-02 23:54:40 -0500

    added api

--- src/pages/personalplans.js
+++ src/pages/personalplans.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 function PersonalPlans() {
+    useEffect(() => {   
+        const apiKey = process.env.REACT_APP_API_KEY;
+        console.log('API key:', apiKey);
+    }, []);
+    
     return (
         <div>
             <h2>Personal Plans</h2>
