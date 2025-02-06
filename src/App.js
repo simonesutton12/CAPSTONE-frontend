ca8999262f6db943eaff7b8c7281f7adca1c8958
Simone <simonesutton94@gmail.com>
2025-02-05 20:54:04 -0500

    setup backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Homepage';
 import PersonalPlans from './pages/personalplans';
 import About from './pages/about';
@@ -16,20 +16,19 @@ function App() {
             <div>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/personal-plans">Personal Plans</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/profile">Profile</a></li>
+                        <li><Link to="/">home</Link></li>
+                
+                        <li><Link to="/personal-plans">Personal Plans</Link></li>
+                        <li><Link to ="/about">About</Link></li>
+                        <li><Link to= "/user-profile/">Profile</Link></li>
                     </ul>
                 </nav>
                 <Routes>
-                  <Route path="/custom-meals" element={<CustomMeals />} />
                     <Route path="/" element={<Home />} />
+                    <Route path="/custom-meals" element={<CustomMeals />} />
                     <Route path="/personal-plans" element={<PersonalPlans />} />
                     <Route path="/about" element={<About />} />
-                    <Route path="/profile" element={<UserProfile />} />
-                    <Route path="/" exact component={PersonalPlans} />
-          <Route path="/user-profile/:planId" component={UserProfile} />
+                    <Route path="/user-profile/:planId" component={<UserProfile />} />
                     
                 </Routes>
             </div>
